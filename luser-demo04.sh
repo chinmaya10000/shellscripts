e7a396e7d557eb9ab73873485fe511233961fcbd
chinmaya10000 <chinmayapradhan10000@gmail.com>
2024-02-15 05:48:28 +0000

    initial commit

---
+++ luser-demo04.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This scripts create an account on the local system.
+# You will prompted for the account name and password.
+
+# Ask for the username.
+
+
+# Ask for the real name.
+
+# Ask for the password.
+
+# Create the user.
+
+# Set the password for the user.
+
+# Force password change on first login.
+
